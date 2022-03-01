3301c3e5cafb65568e11231944690cc3e4ac8218
finn0529 <rockerman0074@gmail.com>
2022-03-01 17:12:58 +0800

    env

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,6 +7,7 @@
 // https://v1.quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 const ESLintPlugin = require('eslint-webpack-plugin')
+const env = require('dotenv').config().parsed
 
 module.exports = function (/* ctx */) {
   return {
@@ -52,8 +53,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       env: {
-        ...require('dotenv').config().parsed,
-        VUE_APP_API:process.env.VUE_APP_API
+        VUE_ENV_API: env?.VUE_ENV_API || process.env.VUE_ENV_API
         },
       extendWebpack (cfg) {
         cfg.module.rules.push({
